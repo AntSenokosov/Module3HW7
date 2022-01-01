d75c0142ec0d462d5d3bf600e451b8256d4c27d7
AntSenokosov <a.y.senokosov@gmail.com>
2022-01-01 21:47:10 +0200

    Add folder Abstract in folder Service and add interface ILoggerService

---
+++ Module3HW7/Module3HW7/Services/Abstract/ILoggerService.cs
@@ -0,0 +1,9 @@
+﻿namespace Module3HW7.Services.Abstract;
+
+public interface ILoggerService
+{
+    public void LogError(string message);
+    public void LogInfo(string message);
+    public void LogWarning(string message);
+    public void WriteFile(string filePath);
+}
