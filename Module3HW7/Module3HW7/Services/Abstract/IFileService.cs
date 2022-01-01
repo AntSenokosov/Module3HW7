4071c29934c6298a87b848080473bb6c6ff127b5
AntSenokosov <a.y.senokosov@gmail.com>
2022-01-01 22:25:39 +0200

    Add interface IFileService in folder Abstract Service

---
+++ Module3HW7/Module3HW7/Services/Abstract/IFileService.cs
@@ -0,0 +1,9 @@
+﻿namespace Module3HW7.Services.Abstract;
+
+public interface IFileService
+{
+    public string ReadFile(string path);
+    public IDisposable CreateFile(string path);
+    public void WriteFile(IDisposable streamWriter, string text);
+    public void CloseFile(IDisposable streamWriter);
+}
