577615c110cac76d7b316ffde89f528f50f55cf2
AntSenokosov <a.y.senokosov@gmail.com>
2022-01-02 12:13:58 +0200

    Add class Actions

---
+++ Module3HW7/Module3HW7/Actions.cs
@@ -0,0 +1,31 @@
+﻿using Module3HW7.Helpers;
+using Module3HW7.Services.Abstract;
+
+namespace Module3HW7;
+
+public class Actions
+{
+    private readonly ILoggerService _logger;
+
+    public Actions(ILoggerService logger)
+    {
+        _logger = logger;
+    }
+
+    public bool ErrorMethod()
+    {
+        throw new Exception("I broke a logic");
+    }
+
+    public bool InfoMethod()
+    {
+        _logger.LogInfo("Start method: " + nameof(InfoMethod));
+
+        return true;
+    }
+
+    public bool WarningMethod()
+    {
+        throw new BusinessException("Skipped logic in method");
+    }
+}
